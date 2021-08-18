4c6031a993cc5dd949d23dd3a83e1cdf62a2db11
Rose Kamal Love <rosekamallove@gmail.com>
2021-08-18 12:48:33 +0530

    day 3

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,35 @@
-let number = 0;
-
 /**
- * You will be given a number,
- * if it is divisble by 3 then print "Fizz",
- * else if it is divisble by 5 print "Buzz",
- * else if it is divisible by both 5 and 3, pring "FizzBuzz",
- * else print the number itself.
+ * const, let, var
+ * js objects
+ * document object model
+ * arrays
+ * es6 arrow functions
  */
 
-// number = prompt("Enter the number");
+const linkPanel = document.querySelector("#addLinksPanel");
+const addBtn = document.querySelector("#addBtn");
+const cancelButton = document.querySelector("#cancelButton");
+const submitButton = document.querySelector("#submitButton");
 
-for (let i = 0; i < 100; i++) {
-  number = i;
-  if (isDivisible(number, 3) && isDivisible(number, 5)) {
-    console.log("Fizz");
-  } else if (isDivisible(number, 5)) {
-    console.log("Buzz");
-  } else if (isDivisible(number, 3)) {
-    console.log("FizzBuzz");
-  } else {
-    console.log(number);
-  }
-}
+let links = [];
 
-function isDivisible(a, b) {
-  if (a % b === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
+addBtn.addEventListener("click", function () {
+  linkPanel.classList.remove("hidden");
+});
+
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  let title = linkTitle.value;
+  let url = linkUrl.value;
+  const newLink = { title: title, url: url };
+  links.push(newLink);
+  linkTitle.value = "";
+  linkUrl.value = "";
+  categories.value = "";
+});
+
+cancelButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  linkPanel.classList.add("hidden");
+});
